6bbbb6cbd61a76ad4bb83a0e2bb42821a05f1bea
Peter Kokalov <petervkokalov@gmail.com>
2017-12-15 16:57:01 -0800

    Update Prepro.py

--- Prepro.py
+++ Prepro.py
@@ -1,8 +1,10 @@
+import pandas as pd
 import pickle
 import bs4 as bs
 import os
 from os.path import exists
 import requests
+import sys
 
 from googlefinance.client import get_prices_time_data
 import quandl
@@ -23,6 +25,63 @@ def update_ticks():
         pickle.dump(tickers, f)
 
     return tickers
+
+### quandl may work for goog and googl but not necessary to use
+def quandl_pull(api_key):
+    quandl.ApiConfig.api_key = api_key
+
+    if not os.path.exists('stock_dfs'):
+        os.makedirs('stock_dfs')
+    else:
+        print('Directory Exists')
+    ticks = [
+        'GOOG',
+        'GOOGL'
+             ]
+
+    for stock in ticks:
+        if not exists('SP500/{}.csv'.format(stock)):
+            try:
+                data = quandl.get('EOD/{}'.format(stock),
+                                  paginate=True,
+                                  start_date="2010-01-01",
+                                  end_date="2017-12-13")
+                drop_cols = [name for name in data.columns if name != 'Close']
+                data.drop(labels=drop_cols, inplace=True, axis=1)
+                data.rename(columns={'Close':stock}, inplace=True)
+                data.to_csv('stock_dfs/{}.csv'.format(stock))
+                print("Adding {} to stock_dfs directory".format(stock))
+            except:
+                print("Unable to read URL for: {}".format(stock))
+
+### putting into 1 df
+def single_df():
+
+    missing_ticks = []
+    with open('SP500quotes.pickle', 'rb') as f:
+        tickers = pickle.load(f)
+
+    main_df = pd.DataFrame()
+
+    for stock in tickers:
+        try:
+            df = pd.read_csv('stock_dfs/{}.csv'.format(stock))
+            if main_df.empty:
+                main_df = df
+            else:
+                if '2009-12-10' in str(df.head(1)['Date']) and '2017-12-13' in str(df.tail(1)['Date']):
+                    main_df = main_df.merge(df)
+                else:
+                    missing_ticks.append(stock)
+        except FileNotFoundError:
+            print("No csv for {}".format(stock))
+            missing_ticks.append(stock)
+    print(main_df.shape)
+    try:
+        main_df.to_csv('Joined_Closes.csv')
+        print("CSV with joined closes saved as 'Joined_Closes.csv'.")
+    except:
+        print("Unable to save CSV of joined closes.")
 
 ###: googlefinance.client having issues with pulling historicals
 def goog_pull(request_again):
@@ -56,6 +115,7 @@ def goog_pull(request_again):
                     '{}_Open'.format(stock),
                     '{}_High'.format(stock),
                     '{}_Low'.format(stock),
+                    '{}_Volume'.format(stock),
                 ]
                 ticker.drop(drop_labs, axis=1, inplace=True)
                 ticker.rename(columns={'{}_Close'.format(stock):stock}, inplace=True)
@@ -80,9 +140,11 @@ def alpha_vantage_pull(request_again):
     for stock in tickers:
         if not exists('SP500/{}.csv'.format(stock)):
             try:
-                ts = TimeSeries(key='C98CEUQBPZ9L15IV', output_format='pandas')
-                ticker, meta_data = ts.get_daily(symbol=stock, outputsize="full")
-                ticker = ticker.iloc[2300:]
+                ts = TimeSeries(key='C98CEUQBPZ9L15IV',
+                                output_format='pandas')
+                ticker, meta_data = ts.get_daily(symbol=stock,
+                                                 outputsize="full")
+                ticker = ticker.iloc[2500:]
                 drop_labs = [
                     'open',
                     'high',
@@ -91,78 +153,20 @@ def alpha_vantage_pull(request_again):
                 ]
                 ticker.drop(drop_labs, axis=1, inplace=True)
                 ticker.rename(columns={'close':stock}, inplace=True)
-                ticker.to_csv('stock_dfs/{}.csv'.format(stock))
-                print("Adding {} to stock_dfs directory".format(stock))
-            except:
-                print("Unable to read URL for: {}".format(stock))
-
-def quandl_pull(request_again, api_key):
-    quandl.ApiConfig.api_key = api_key
-    if request_again == True:
-        tickers = update_ticks()
-
-    else:
-        with open('SP500quotes.pickle', 'rb') as f:
-            tickers = pickle.load(f)
-
-    if not os.path.exists('stock_dfs'):
-        os.makedirs('stock_dfs')
-    else:
-        print('Directory Exists')
-
-    for stock in tickers:
-        if not exists('SP500/{}.csv'.format(stock)):
-            try:
-                data = quandl.get('EOD/AAPL',
-                                  paginate=True,
-                                  start_date="2010-01-01",
-                                  end_date="2017-12-13")
-                drop_cols = [name for name in data.columns if name != 'Close']
-                data.drop(labels=drop_cols, inplace=True, axis=1)
-                data.rename(columns={'Close':stock}, inplace=True)
-                data.to_csv('stock_dfs/{}.csv'.format(stock))
-                print("Adding {} to stock_dfs directory".format(stock))
-            except:
-                print("Unable to read URL for: {}".format(stock))
-
-def single_df():
-
-    with open('SP500quotes.pickle', 'rb') as f:
-        tickers = pickle.load(f)
-
-    # tickers.append('comps')
-    main_df = pd.DataFrame()
-
-    for stock in tickers:
-        try:
-
-            df = pd.read_csv('stock_dfs/{}.csv'.format(stock))
-
-            print(df.head())
-            print(df.tail())
-
-            # df.reset_index(inplace=True)
-            df.rename(columns={'Unnamed: 0':'Periods'}, inplace=True)
-            period_start = str(df['Periods'][0])
-            period_end = str(df.tail(1)['Periods'])
-
-            if '2017-12-11' in period_end and '2007-11-27' in period_start:
-                if main_df.empty:
-                    main_df = df
-
+                if ticker.empty:
+                    print("Empty dataframe for {} - will not add to stocks_dfs dir".format(stock))
                 else:
-                    main_df = main_df.merge(df)
-            break
-        except:
-            pass
-
-    # main_df.to_csv('Joined_Closes.csv')
+                    ticker.to_csv('stock_dfs/{}.csv'.format(stock))
+                    print("Adding {} to stock_dfs directory".format(stock))
+            except:
+                s = "Error: {}".format(sys.exc_info()[0])
+                s += "\nwhile fetching data for {}".format(stock)
 
 def currency_data():
     fred = Fred(api_key='cf154315e654c009c1b944f20dd1e028')
 
     USEUROforex = fred.get_series('DEXUSEU')
-    USEUROforex = pd.DataFrame(USEUROforex).iloc[2321:]
+    USEUROforex = pd.DataFrame(USEUROforex)
     USEUROforex.columns = ['USvsEURO']
 
     USEUROforex.fillna(
@@ -174,14 +178,10 @@ def currency_data():
 
     exchange_indices = [str(ix)[0:10] for ix in USEUROforex.index.values]
     JC_indices = [ix[0:10] for ix in JC['Periods']]
-
-    labs = []
-    for i in exchange_indices:
-        if i not in JC_indices:
-            labs.append(i)
-
+    labs = [i for i in exchange_indices if i not in JC_indices]
     USEUROforex.reset_index(inplace=True)
     USEUROforex_dict = {'USvsEURO':[]}
+
     for i, ix in enumerate(USEUROforex['index']):
         if str(ix)[0:10] in labs:
             pass
@@ -216,15 +216,9 @@ def sectors():
             "{}_Open".format(name),
             "{}_High".format(name),
             "{}_Low".format(name),
+            "{}_Volume".format(name),
         ],
             axis=1, inplace=True
         )
     print(comparables.head())
     # comparables.to_csv('Sectors.csv')
-
-quandl_pull(request_again=False, api_key="UvnTzbiRyVMTwtkL26iK")
-#alpha_vantage_pull(request_again=False)
-#goog_pull(request_again=True)
-# single_df()
-# currency_data()
-#sectors()
